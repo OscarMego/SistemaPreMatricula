51f388489b068154af2b88078ccdcbb48cc34c6c
antoniofb3 <antoniofb3@gmail.com>
2019-04-27 17:58:34 -0500

    Adding ConsultarCursos navigation forms

---
+++ SistemaDePrematricula/ConsultarCursos.aspx.cs
@@ -0,0 +1,20 @@
+﻿using SistemaDePrematricula.Dominio;
+using SistemaDePrematricula.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SistemaDePrematricula
+{
+    public partial class ConsultarCursos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
